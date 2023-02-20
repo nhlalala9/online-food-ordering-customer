7e6fb0602342095797f45451793388632425184a
Oratile1234 <oratilemabote16@gmail.com>
2023-02-20 16:28:15 +0200

    intial

--- online-food-ordering-customer/src/environments/environment.ts
+++ online-food-ordering-customer/src/environments/environment.ts
@@ -1,9 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+const appUrl = "http://localhost:1337/api/"
 export const environment = {
-  production: false
+  production: false,
+  appUrl: `${appUrl}`
 };
 
 /*
