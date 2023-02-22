134cb8daf323ad85397fb6000958e8821a00d235
Oratile1234 <oratilemabote16@gmail.com>
2023-02-22 15:01:08 +0200

    customer dashboard

--- online-food-ordering-customer/src/app/app.module.ts
+++ online-food-ordering-customer/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';    
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';   
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -28,7 +27,6 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    NgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
