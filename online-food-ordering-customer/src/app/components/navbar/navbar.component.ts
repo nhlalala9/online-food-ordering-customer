c4aea53a78590da651489aa5fcc85cef27cd5e5f
Oratile1234 <oratilemabote16@gmail.com>
2023-03-06 12:04:33 +0200

    before

--- online-food-ordering-customer/src/app/components/navbar/navbar.component.ts
+++ online-food-ordering-customer/src/app/components/navbar/navbar.component.ts
@@ -10,10 +10,42 @@ export class NavbarComponent implements OnInit {
 
   public totalItem : number = 1;
   public searchTerm !: string;
+  getCartDetails: any[] = [];
+  total: number = 0;
+  cartNumber: number = 0;
+  
   constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
-   
+    this.loadCart();
+  }
+  loadCart(): void {
+    const cartData = localStorage.getItem('localCart');
+    console.log(cartData,"me");
+
+    
+    
+    if (cartData) {
+      this.getCartDetails = JSON.parse(cartData);
+      console.log(this.getCartDetails,"why");
+      this.total = this.getCartDetails.reduce((acc, val) => acc + (val.attributes?.price * val.attributes?.quantity), 0);
+      console.log(this.total,"total")
+      localStorage.setItem('total', JSON.stringify(this.total));
+      console.log(this.total)
+    } else {
+      this.getCartDetails = [];
+      this.total = 0;
+    }
+    this.cartNumberFunc();
   }
 
+  cartNumberFunc(): void {
+    const cartData = localStorage.getItem('localCart');
+    if (cartData) {
+      this.cartNumber = JSON.parse(cartData).length;
+    } else {
+      this.cartNumber = 0;
+    }
+    this.cartService.addtoCart(this.cartNumber);
+  }
 }
