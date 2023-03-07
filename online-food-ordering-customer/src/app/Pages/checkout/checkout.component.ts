0b9447903ed6c8ed9acd8a6921b5b33bd5e5a985
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-03-07 10:35:42 +0200

    payment

--- online-food-ordering-customer/src/app/Pages/checkout/checkout.component.ts
+++ online-food-ordering-customer/src/app/Pages/checkout/checkout.component.ts
@@ -9,6 +9,7 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  paymentHandler: any = null;
 
   constructor(private datePipe: DatePipe,private cartService : CartService,private fb: FormBuilder,private checkoutService : CheckoutService) { }
   public checkoutForm: FormGroup = this.fb.group({
@@ -88,6 +89,43 @@ export class CheckoutComponent implements OnInit {
     this.checkoutForm.reset();
     localStorage.removeItem('localCart');
     this.cartService.addtoCart(0);
+  }
+
+
+  // for payment
+  makePayment(amount: any) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51H7bbSE2RcKvfXD4DZhu',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken);
+        alert('Stripe token generated!');
+      },
+    });
+    paymentHandler.open({
+      name: 'Positronx',
+      description: '3 widgets',
+      amount: amount * 100,
+    });
+  }
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51H7bbSE2RcKvfXD4DZhu',
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken);
+            alert('Payment has been successfull!');
+          },
+        });
+      };
+      window.document.body.appendChild(script);
+    }
   }
 
 }
