ff52411f8483aef676fcf2a86e975341f6aa4761
Oratile1234 <oratilemabote16@gmail.com>
2023-03-08 10:16:38 +0200

    nav

--- online-food-ordering-customer/src/app/components/register/register.component.ts
+++ online-food-ordering-customer/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthenticationService } from '../../service/authentication.service';
